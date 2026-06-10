d3509855cedfcbfd6cc3b18a155125d96df14f06
yilishawk <51566598+yilishawk@users.noreply.github.com>
2026-06-10 13:02:34 +0800

    Update Wooyun.java

--- app/src/main/java/com/github/catvod/spider/Wooyun.java
+++ app/src/main/java/com/github/catvod/spider/Wooyun.java
@@ -16,6 +16,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
