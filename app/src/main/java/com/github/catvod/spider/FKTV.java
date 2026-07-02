ccf2ea3e0a140d5d905e9128a1c370feee8c7225
yilishawk <51566598+yilishawk@users.noreply.github.com>
2026-07-02 18:39:21 +0800

    Update FKTV.java

--- app/src/main/java/com/github/catvod/spider/FKTV.java
+++ app/src/main/java/com/github/catvod/spider/FKTV.java
@@ -11,7 +11,7 @@ import org.json.JSONObject;
 import java.util.*;
 import java.util.regex.*;
 
-public class FanKeSpider extends com.github.catvod.crawler.Spider {
+public class FKTV extends com.github.catvod.crawler.Spider {
 
     private final String HOST = "https://fktv.me";
 
