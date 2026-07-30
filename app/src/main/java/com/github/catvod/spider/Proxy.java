72a67d5998ef7e6ed8ef57abfcbff8a6e5e4455e
yilishawk <51566598+yilishawk@users.noreply.github.com>
2026-07-30 11:24:54 +0800

    Update Proxy.java

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -20,7 +20,7 @@ import java.util.regex.Pattern;
 public class Proxy {
 
     private static final int PROXY_PORT = 9978;
-    public static final StringBuilder sb = new StringBuilder("<div style='color:#888;'>--- 凱哥全能矩陣引擎已啟動---</div>");
+    public static final StringBuilder sb = new StringBuilder("<div style='color:#888;'>---凱哥全能矩陣引擎已啟動---</div>");
 
     public static int getPort() {
         return PROXY_PORT;
