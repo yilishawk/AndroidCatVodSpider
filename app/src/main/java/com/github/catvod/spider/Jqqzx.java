4a71e26c536d058aa869aa72d6c2ad013c3d85f5
yilishawk <51566598+yilishawk@users.noreply.github.com>
2026-09-21 12:04:49 +0800

    Update Jqqzx.java

--- app/src/main/java/com/github/catvod/spider/Jqqzx.java
+++ app/src/main/java/com/github/catvod/spider/Jqqzx.java
@@ -17,6 +17,7 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.net.URLEncoder;
 import java.security.MessageDigest;
